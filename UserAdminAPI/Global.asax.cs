cbf72a2612720c6e4353bf19b21f7fe708ed0e68
Fedyukovda <msh3000@yandex.ru>
2017-09-29 19:09:38 +0500

    Добавьте файлы проекта.

---
+++ UserAdminAPI/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using UserAdminAPI.Models;
+
+namespace UserAdminAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new UserDbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
